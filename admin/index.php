888861065a136e981d40289c2617fad9eb6a1566
waithethtetoo <4itcourse@gmail.com>
2020-10-05 16:16:14 +0630

    add index page

---
+++ admin/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Book Store</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+<h1>Login to Book Store Administration</h1>
+<form action="login.php" method="post">
+	<label for="name">Name</label>
+	<input type="text" id="name" name="name">
+
+	<label for="password">Password</label>
+	<input type="password" id="password" name="password">
+
+	<br><br>
+	<input type="submit" value="Admin Login">
+</form>
+</body>
+</html>

03828e3e6b4bcba1f9d1f0e70bb96449d9d9adc8
waithethtetoo <4itcourse@gmail.com>
2020-10-05 16:13:36 +0630

    change category new page
    link with style.css
    add menu

--- admin/cat-new.php
+++ admin/cat-new.php
@@ -1,12 +1,10 @@
+<?php include("confs/auth.php") ?>
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Book Store</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<style>
-		body{
-			background: #A8EFA6;
-			font-family: arial;
-		}
 		form label {
 			display: block;
 			margin-top: 8px;
@@ -15,15 +13,22 @@
 </head>
 <body>
 	<h1>New Category</h1>
+	<ul class="menu">
+		<li><a href="book-list.php">Manage Books</a></li>
+		<li><a href="cat-list.php">Manage Categories</a></li>
+		<li><a href="rental.php">Rental Books</a></li>
+		<li><a href="logout.php">Logout</a></li>
+	</ul>
 	<form action="cat-add.php" method="post">
-		<label for="name"> Category name </label>
+		<label for="name">Category Name</label>
 		<input type="text" name="name" id="name">
 
 		<label for="remark">Remark</label>
 		<textarea name="remark" id="remark"></textarea>
-
+		
 		<br><br>
 		<input type="submit" value="Add Category">
+		<a href="cat-list.php" class="back">Back</a>
 	</form>
 </body>
 </html>

4d5085d4c7063913180308a37d634ba138129c1e
waithethtetoo <4itcourse@gmail.com>
2020-10-05 16:03:00 +0630

    change category list page
    link with css page.
    add menu.
    link to category delete and edit page.

--- admin/cat-list.php
+++ admin/cat-list.php
@@ -1,16 +1,18 @@
+<?php include("confs/auth.php") ?>
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Book Store</title>
-	<style>
-		body{
-			background: #A8EFA6;
-			font-family: arial;
-		}
-	</style>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
 	<h1>Category List</h1>
+	<ul class="menu">
+		<li><a href="book-list.php">Manage Books</a></li>
+		<li><a href="cat-list.php">Manage Categories</a></li>
+		<li><a href="rental.php">Rental Books</a></li>
+		<li><a href="logout.php">Logout</a></li>
+	</ul>
 	<?php
 		include("confs/config.php");
 		$result = mysqli_query($conn,"SELECT * FROM categories");
@@ -18,6 +20,8 @@
 		<ul>
 			<?php while ($row = mysqli_fetch_assoc($result)): ?>
 				<li title="<?php echo $row['remark'] ?>">
+				[<a href="cat-delete.php?id=<?php echo $row['id'] ?>" class="del">delete</a>]
+				[<a href="cat-edit.php?id=<?php echo $row['id'] ?>" class="edit">edit</a>]
 				<?php echo $row['name'] ?>
 				</li>
 			<?php endwhile; ?>

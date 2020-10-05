730bc89a257930e0eebb2bc29faf4ed0030ebc6b
waithethtetoo <4itcourse@gmail.com>
2020-10-05 16:01:06 +0630

    add categrogy add page

--- admin/cat-add.php
+++ admin/cat-add.php
@@ -1,10 +1,10 @@
 <?php
-	include("confs/config.php");
-	$name = $_POST['name'];
-	$remark = $_POST['remark'];
-	$sql = "INSERT INTO categories (name,remark,created_date,modified_date) VALUES ('$name','$remark',now(),now())";
+include("confs/config.php");
+$name = $_POST['name'];
+$remark = $_POST['remark'];
 
-	mysqli_query($sql);
+$sql = "INSERT INTO categories(name, remark, created_date, modified_date) VALUES('$name','$remark',now(),now())";
 
-	header("location: cat-list.php");
+mysqli_query($conn,$sql);
+header("location: cat-list.php");
 ?>
